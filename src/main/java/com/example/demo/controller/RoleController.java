699d4949962407d86d3552f6991f9a217221e726
Boburjon <soliyevboburjon95@gmail.com>
2023-09-22 22:43:53 +0500

    Uzum_Market_With_Spring_Bood

--- src/main/java/com/example/demo/controller/RoleController.java
+++ src/main/java/com/example/demo/controller/RoleController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@PreAuthorize("hasAnyRole('SUPER_ADMIN','ADMIN')")
 public class RoleController {
 }
--- src/main/java/com/example/demo/controller/StatusController.java
+++ src/main/java/com/example/demo/controller/StatusController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@PreAuthorize("hasAnyRole('ADMIN','SUPER_ADMIN')")
 public class StatusController {
 }
