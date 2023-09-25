1d81f7a7ef00f6c103c9c647d6e3e75a26f2943e
Boburjon <soliyevboburjon95@gmail.com>
2023-09-25 06:06:46 +0500

    Uzum_Market_With_Spring_Bood

--- src/main/java/com/example/demo/repository/GoodRepository.java
+++ src/main/java/com/example/demo/repository/GoodRepository.java
@@ -29,7 +29,9 @@ public interface GoodRepository extends JpaRepository<Good, UUID>, JpaSpecificat
     @Query(value = "update good set blocked=?1 where id=?2")
     void updateGoodBlockedById(boolean blocked, UUID id);
 
-    @Query(nativeQuery = true,value = "select * from good g inner join color c on g.blocked=false and lower(c.name) = any(:names) inner join type t on g.blocked=false and lower(t.name) = any(:names) where lower(g.name) = any(:names) and ((:prices) is null or g.price = any(:prices))")
+//    @Query(nativeQuery = true,value = "select * from good g inner join color c on g.blocked=false and lower(c.name) = any(:names) inner join type t on g.blocked=false and lower(t.name) = any(:names) where lower(g.name) = any(:names) and ((:prices) is null or g.price = any(:prices))")
+//    Page<Good> findAllByName(List<String> names, List<Double> prices, Pageable pageable);
+    @Query(nativeQuery = true,value = "select * from good g inner join color c on g.blocked=false and lower(c.name) like any(:names) inner join type t on g.blocked=false and lower(t.name) like any(:names) where lower(g.name) like any(:names) and ((:prices) is null or g.price like any(:prices))")
     Page<Good> findAllByName(List<String> names, List<Double> prices, Pageable pageable);
     @Query(nativeQuery = true,value = "SELECT g FROM public.good g WHERE g.blocked=false and (?1 IS NULL OR g.color_id = ?1) AND(?2 IS NULL OR g.price >= ?2) AND(?3 IS NULL OR g.price <= ?3) AND(?4 IS NULL OR g.type_id = ?4)")
     Page<Good> findByCriteria(Long colorId, Double startPrice, Double endPrice, Long typeId, Pageable pageable);
