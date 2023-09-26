0196eebf7cc47e3797ae576a3771302481f4d306
Boburjon <soliyevboburjon95@gmail.com>
2023-09-26 15:22:48 +0500

    Uzum_Market_With_Spring_Bood

--- src/main/java/com/example/demo/controller/MultimediaController.java
+++ src/main/java/com/example/demo/controller/MultimediaController.java
@@ -15,7 +15,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Mono;
-
 import java.io.File;
 import java.net.MalformedURLException;
 import java.nio.file.Path;
